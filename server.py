0b37e55a46dfaa40d5fa9cdb04d9127385acf8e5
Cher Lafferty <cherlafferty01@gmail.com>
2022-01-21 17:13:56 -0800

    crud

--- server.py
+++ server.py
@@ -5,11 +5,8 @@ from users import User
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def index():
-    return redirect('/users')
-
-
 @app.route('/users')
 def users():
     return render_template("users.html", users=User.get_all())
@@ -25,6 +22,33 @@ def create():
     User.save(request.form)
     return redirect('/users')
     #Note, remember, never 'render' on a POST
+
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data = {
+        "id" : id
+    }
+    return render_template("edit_user.html", user=User.get_one(data))
+
+@app.route('/user/show/<int:id>')
+def show(id):
+    data = {
+        "id" : id
+    }
+    return render_template("show_user.html",user=User.get_one(data))
+
+@app.route('/user/update', methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data = {
+        'id' : id
+    }
+    User.destroy(data)
+    return redirect('/users')
 
 
 if __name__ == "__main__":
--- users.py
+++ users.py
@@ -10,6 +10,11 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
     # Now we use class methods to query our database
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
@@ -26,4 +31,21 @@ class User:
     def save(cls, data ):
         query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(first_name)s , %(last_name)s , %(email)s , NOW() , NOW() );"
         # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('users').query_db( query, data )
+
+    @classmethod
+    def get_one(cls,data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL('users').query_db(query,data)
+        #result is a list thus []
+        return cls(result[0])
+
+    @classmethod
+    def update(cls,data):
+        query = "UPDATE users SET first_name=%(first_name)s,last_name=%(last_name)s,email=%(email)s,updated_at=NOW() WHERE id =%(id)s;"
+        return connectToMySQL('users').query_db( query, data )
+
+    @classmethod
+    def destroy(cls,data):
+        query = "DELETE FROM users WHERE id =%(id)s;"
         return connectToMySQL('users').query_db( query, data )
